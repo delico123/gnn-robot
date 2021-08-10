10e68998ef30514935d42a89f52b3f698c489046
Joanne T. Kim <33623132+delico123@users.noreply.github.com>
2021-08-10 11:04:27 -0400

    Updated data and data gen (Final?!)

--- script_data_gen.sh
+++ script_data_gen.sh
@@ -1,4 +1,9 @@
 # Generate data. Structure
 cd ./reacher_simulate
 
-python main.py --str_only --num_urdf 1000 --min_njoint 2 --max_njoint 7
+python main.py --str_only --num_urdf 1000 --min_njoint 2 --max_njoint 7 --xml_dir="./xml"
+python main.py --dyn_only --num_urdf 100 --num_iter 100 --min_njoint 2 --max_njoint 7 --xml_dir="./xml"
+
+
+# python main.py --str_only --num_urdf 1000 --min_njoint 2 --max_njoint 4 --xml_dir="./xml_fix_half" --fix_len
+# python main.py --dyn_only --num_urdf 100 --num_iter 100 --min_njoint 2 --max_njoint 4 --xml_dir="./xml_fix_half" --fix_len
